302c111cf24ecfc4dc20a2f2b0f392fa5b8cadc3
Sarah Lim <sarah@sarahlim.com>
2016-06-17 23:40:58 -0700

    separate functions into own directory

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,3 +1,7 @@
+############################################
+# Navigation shortcuts
+############################################
+
 # Easier navigation
 # from paulirish dotfiles
 alias ..='cd ..'
@@ -11,41 +15,45 @@ alias dl="cd ~/Downloads"
 # Tree listing of depth 3
 alias tr='tree -L 3'
 
-# Automatically list files after cd
-alias cd='cdl'
-
-# Use ccat for syntax highlighted cat
-alias cat='ccat'
-
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-	colorflag="--color"
-else # macOS `ls`
-	colorflag="-G"
-fi
-
 # List files with -F decorative endings
-alias ls='ls -F ${colorflag}'
+alias ls='ls -F'
 
 # List files with -F decorative endings, including hidden files
-alias lsa='ls -Fa ${colorflag}'
-
-# List only directories
-alias lsd='ls -lF ${colorflag} | grep --color=never "^d"'
+alias lsa='ls -Fa'
 
 # List files long with endings, human readable sizes
-alias lsl='ls -hlF ${colorflag}$'
+alias lsl='ls -hlF'
 
 # List files long with endings, human readable sizes, including hidden files
-alias lsla='ls -halF ${colorflag}$'
+alias lsla='ls -halF'
 
 # Enable aliases to be sudo'd
 alias sudo='sudo '
 
+############################################
+# Visual
+############################################
+
+# Use ccat for syntax highlighted cat
+alias cat='ccat'
+
+# Prettyprint path
+alias path='echo $PATH | tr -s ":" "\n"'
+
+############################################
+# Utility
+############################################
+
 # Stopwatch
 alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
 
-# git shortcuts
+# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
+############################################
+# git
+############################################
+
 alias g='git'
 alias gs='git status '
 alias ga='git add '
@@ -54,9 +62,3 @@ alias gc='git commit -m '
 alias gca='git commit -am '
 alias gd='git diff'
 alias go='git checkout '
-
-# Prettyprint path
-alias path='echo $PATH | tr -s ":" "\n"'
-
-# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
