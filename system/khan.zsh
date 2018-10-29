0c0c9c8fc7dde098bd47cf9882baf6eb687effa5
Sarah Lim <sarah@khanacademy.org>
2018-10-29 14:40:01 -0700

    Add aliases from internship

--- system/khan.zsh
+++ system/khan.zsh
@@ -1,7 +1,16 @@
 # Khan Academy configuration.
 
-# Source virtualenv (since khan-dotfiles are designed for bash).
+# Source sh-agnostic profile
+source ~/khan/devtools/khan-dotfiles/.profile.khan
+
+# Source virtualenv for KA
 source ~/.virtualenv/khan27/bin/activate
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/sarahlim/exec -l /bin/bash/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/sarahlim/exec -l /bin/bash/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/sarahlim/exec -l /bin/bash/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/sarahlim/exec -l /bin/bash/google-cloud-sdk/completion.zsh.inc'; fi
 
 # Git aliases
 alias go='git co'
